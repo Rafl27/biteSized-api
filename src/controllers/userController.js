e4450696d47a14db9248a63c4941f5d57c637922
Rafl27 <raflsouza27@gmail.com>
2023-05-06 13:09:05 -0300

    auth http messages

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,15 +6,37 @@ require('dotenv').config();
 exports.signup = async (req, res) => {
     try {
         const { name, email, password } = req.body;
+
+        // Check for Required Fields
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "Please provide all required fields" });
+        }
+
+        // Check if user already exists
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "A user with this email already exists" });
+        }
+
         const hash = await bcrypt.hash(password, 10);
         const user = new User({ name, email, password: hash });
         await user.save();
+
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
         res.json({ token });
+
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        // Catch Specific Errors
+        if (err.code === 11000) {
+            return res.status(400).json({ message: "A user with this email already exists" });
+        }
+
+        // Handle Unexpected Errors
+        console.error(err);
+        res.status(500).json({ message: "Something went wrong on the server-side" });
     }
 };
+
 
 exports.login = async (req, res) => {
     try {
